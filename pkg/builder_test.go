8c4e4a74c1785de832b1366f897e951054f99e2b
Aryan <collaborativeroom@gmail.com>
2019-11-07 16:32:21 +0330

    add bind

--- pkg/builder_test.go
+++ pkg/builder_test.go
@@ -3,49 +3,32 @@ package gql
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
+
+
 func TestMysqlBuilder(t *testing.T) {
-	r := Read("files f").
-		Join(Query(func(b Builder) {
-			b.Table("hello")
-		}, "t"), "t.user_id = f.id").
-		GroupBy("f.id").
-		Query()
-	fmt.Println(r)
-	c := Create("files").
-		Fill(
-			&OBJ{
-				"hello": 1,
-				"name":  "aryan",
-			},
-			&OBJ{
-				"hello": 55,
-				"name":  "arash",
-			},
-		).
-		Query()
-	fmt.Println(c)
-	u := Update("files").
-		Fill(&OBJ{
-			"hello": 123,
-			"name":  "aryan",
-		}).
-		Where("id", 2).
-		Query()
-	fmt.Println(u)
-	d := Delete("files").
-		Fill(&OBJ{
-			"hello": 123,
-			"name":  "aryan",
-		}).
-		Where("id", 2).
-		Query()
-	fmt.Println(d)
-	h := Table("files").
-		Field("id", "int not null primary key auto_increment").
-		Field("name", "varchar(100)").
-		ForeignKey("name", "users", "id").
-		Query()
-	fmt.Println(h)
+	type File struct {
+		Name     string     `gql:"fnam"`
+		JobTitle string     `gql:"jtitl"`
+		Hint     string     `gql:"hint"`
+		Hello     int64
+		Time     *time.Time `gql:"tm"`
+	}
+	file := []*File{
+		{
+			Name: "aryan",
+			JobTitle: "developer",
+			Hint: "hello",
+		},
+		{
+			Name:     "jacob",
+			JobTitle: "manager",
+			Hint: "bye",
+		},
+	}
+	a := Create("files").BindExclude(&file, "hello").Run()
+	fmt.Println(a.GetError())
+	fmt.Println(a.Query())
 }
--- pkg/intfc_to_sql.go
+++ pkg/intfc_to_sql.go
@@ -11,46 +11,60 @@ import (
 func float_to_string(f float64) string {
 	return strconv.FormatFloat(f, 'f', -1, 64)
 }
-func interface_to_sql(value interface{}) string {
+func interface_to_sql(value interface{}) (out string) {
+	defer func() {
+		if p:= recover(); p != nil {
+			out = "NULL"
+		}
+	}()
 	if value == nil {
-		return "NULL"
+		out =  "NULL"
 	}
 	switch value.(type) {
 	case string:
-		return fmt.Sprintf("'%v'", strings.ReplaceAll(value.(string), "'", "\\'"))
+		out =  fmt.Sprintf("'%v'", strings.ReplaceAll(value.(string), "'", "\\'"))
+		return
 	case float32:
-		return fmt.Sprintf("%s", float_to_string(float64(value.(float32))))
+		out =  fmt.Sprintf("%s", float_to_string(float64(value.(float32))))
+		return
 	case float64:
-		return fmt.Sprintf("%s", float_to_string(value.(float64)))
+		out =  fmt.Sprintf("%s", float_to_string(value.(float64)))
+		return
 	case SqlReserved:
-		return (value.(SqlReserved)).content
+		out =  (value.(SqlReserved)).content
+		return
 	case *SqlReserved:
-		return (value.(*SqlReserved)).content
+		out =  (value.(*SqlReserved)).content
+		return
 	case time.Time:
 		d := value.(time.Time)
-		return interface_to_sql(d.Format("2006-01-02 15:04:05"))
+		out =  interface_to_sql(d.Format("2006-01-02 15:04:05"))
+		return
 	case *time.Time:
 		d := value.(*time.Time)
-		return interface_to_sql(d.Format("2006-01-02 15:04:05"))
+		out = interface_to_sql(d.Format("2006-01-02 15:04:05"))
+		return
 	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, bool:
-		return fmt.Sprintf("%v", value)
+		out =  fmt.Sprintf("%v", value)
+		return
 	default:
 		v := reflect.ValueOf(value)
 		if v.Kind() != reflect.Slice {
-			return ""
+			out = ""
+			return
 		}
 		ln := v.Len()
-		op:= "("
+		out = "("
 		for i:=0;i<ln ;i++  {
 			val := interface_to_sql(v.Index(i).Interface())
 			if val != "" {
-				op += val
+				out += val
 				if i != ln - 1 {
-					op += ","
+					out += ","
 				}
 			}
 		}
-		op+=")"
-		return op
+		out+=")"
+		return
 	}
 }
